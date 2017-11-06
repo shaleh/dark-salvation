19ccc04a7e25bc7d32cf8d2142b931fb0c72532a
Sean 'Shaleh' Perry <shaleh@speakeasy.net>
2017-11-05 22:19:44 -0800

    Remove unused bits. Move cygwin out to its own file

--- developer/cgwin.ps1
+++ developer/cgwin.ps1
@@ -1,11 +1,6 @@
 Set-ExecutionPolicy Unrestricted
 
-# Install Developer tools
-cinst gitextensions
-cinst git.install
-cinst python2
-cinst python3
-cinst kdiff3
+cinst cyg-get
 
 # If running in the console, wait for input before closing.
 if ($Host.Name -eq "ConsoleHost")
